e29a5d4151aac89cde26a49df198006634ee0055
Tai Chuan <chuanntph14556@fpt.edu.vn>
2023-03-15 20:56:04 +0700

    Initial commit

---
+++ app/src/main/java/chuannt/ph14556/g5_coffetea/MainActivity.java
@@ -0,0 +1,14 @@
+package chuannt.ph14556.g5_coffetea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
